07ed23a96da125b13d01c62395a5b232141c7b10
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-08-23 01:18:33 +0530

    added the components and modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Hello from Bridgelabz';
+  title = 'latestHelloWorld';
+  imgUrl = '';
+  url = '';
+  userName: string = '';
+  nameError: string = '';
+  ngOnInit(): void {
+    this.title = ' Hello from Bridgelabz';
+    this.imgUrl = '/assets/1555572153465.png';
+    this.url = 'https://www.bridgelabz.com';
+  }
+  onClick($event: any) {
+    console.log('Save button is clicked', $event);
+    window.open(this.url, '_blank');
+  }
+  onInput($event: any) {
+    console.log('Change Event Occurred!', $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s] {2,}$');
+
+    if (nameRegex.test(this.userName)) {
+      this.nameError = '';
+      return;
+    }
+
+    this.nameError = 'Name is Incorrect!';
+  }
 }
